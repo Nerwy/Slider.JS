c90a3107190365636aafaf3c8308cb8c1af9a93b
Nerwy_ <michele.santanche@gmail.com>
2019-04-13 16:20:28 +0200

    Moved next and prev function into the object
    +Added a way to call next and prev from outside the object

--- slider.js
+++ slider.js
@@ -1,9 +1,5 @@
 var sliders = [];
 
-// new SlidersFactory(true, 1, ".slider");
-
-// var test = new SlidersFactory(true, 1, ".sliderslider");
-
 function SlidersFactory(infinite, overflowHidden, sliderItemsPerRow, elmName, animationParams, arrowClassName){
 
     var infinite = (typeof infinite === "boolean") ? infinite : false;
@@ -51,6 +47,8 @@ function SlidersFactory(infinite, overflowHidden, sliderItemsPerRow, elmName, an
 }
 
 function Slider(infinite, overflowHidden, sliderItemsPerRow, elm, animationParams, arrowClassName){
+
+    var self = this;
     
     this.infinite = infinite;
     this.overflowHidden = overflowHidden;
@@ -62,144 +60,129 @@ function Slider(infinite, overflowHidden, sliderItemsPerRow, elm, animationParam
     this.actual = 1;
     this.translateValue = 0;
     this.reduceItemsCountForSliderItemsPerRow = sliderItemsPerRow - 1;
-    // this.next = next;
 
-    sliders.push(this);
+    //SLIDER BUILD
+        sliders.push(this);
 
-    var prevArrow = document.createElement("span"),
-        nextArrow = document.createElement("span");
+        var prevArrow = document.createElement("span"),
+            nextArrow = document.createElement("span");
 
-    prevArrow.classList.add(arrowClassName); prevArrow.classList.add("prev");
-    nextArrow.classList.add(arrowClassName); nextArrow.classList.add("next");
+        prevArrow.classList.add(arrowClassName); prevArrow.classList.add("prev");
+        nextArrow.classList.add(arrowClassName); nextArrow.classList.add("next");
 
-    this.element.prepend(prevArrow);
-    this.element.appendChild(nextArrow);
+        this.element.prepend(prevArrow);
+        this.element.appendChild(nextArrow);
 
-    prevArrow.onclick = prev;
-    nextArrow.onclick = next;
+        prevArrow.onclick = prevItem;
+        nextArrow.onclick = nextItem;
 
-    var sliderItemsOfThisSlider = this.element.querySelectorAll(`#${this.elmName}>div`),
-        sliderItemsCount = sliderItemsOfThisSlider.length,
-        sliderItemsParent = document.createElement("div"),
-        sliderItemsContainer = document.createElement("div");
+        var sliderItemsOfThisSlider = this.element.querySelectorAll(`#${this.elmName}>div`),
+            sliderItemsCount = sliderItemsOfThisSlider.length,
+            sliderItemsParent = document.createElement("div"),
+            sliderItemsContainer = document.createElement("div");
 
-    sliderItemsParent.classList.add("slider_items_parent");
-    sliderItemsContainer.classList.add("slider_items_container");
+        sliderItemsParent.classList.add("slider_items_parent");
+        sliderItemsContainer.classList.add("slider_items_container");
 
-    this.element.insertBefore(sliderItemsParent, nextArrow);
-    sliderItemsParent.appendChild(sliderItemsContainer);
+        this.element.insertBefore(sliderItemsParent, nextArrow);
+        sliderItemsParent.appendChild(sliderItemsContainer);
 
-    
-    sliderItemsParent.style.overflow = (this.overflowHidden) ? "hidden" : "visible"; 
-    sliderItemsContainer.style.width = (sliderItemsCount * 100) / this.sliderItemsPerRow + "%";
-    sliderItemsContainer.style.height = "100%";
-    sliderItemsContainer.style.transition = this.animationParams;
+        sliderItemsParent.style.overflow = (this.overflowHidden) ? "hidden" : "visible"; 
+        sliderItemsContainer.style.width = (sliderItemsCount * 100) / this.sliderItemsPerRow + "%";
+        sliderItemsContainer.style.height = "100%";
+        sliderItemsContainer.style.transition = this.animationParams;
+        var animationTime = this.animationParams.split(" ")[0];
 
-    for (x = 0; x < sliderItemsCount; x++){
-        sliderItemsContainer.appendChild(sliderItemsOfThisSlider[x]);
-        sliderItemsOfThisSlider[x].classList.add("slider_items");
-        sliderItemsOfThisSlider[x].style.width = (100 / sliderItemsCount) + "%";
-    }
-
-}
-
-function prev(){
-
-    getInformationOfSlider.call(this);
-
-    if (sliders[indexOfSlider].actual > 1){
-
-        sliders[indexOfSlider].translateValue += 20;
-        sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" + sliders[indexOfSlider].translateValue + "%)";
-        sliders[indexOfSlider].actual--;
-
-    } else if (sliders[indexOfSlider].actual == 1){
-
-        if (sliders[indexOfSlider].infinite){
-
-            sliderItemsParent.prepend(sliderItems[sliderItemsCount - 1]);
-            sliderItems = sliderParentOfClickedArrow.querySelectorAll(".slider_items");
-
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transitionDuration = "0s";
-            sliders[indexOfSlider].translateValue -= 20;
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" + sliders[indexOfSlider].translateValue + "%)";
-
-            setTimeout(function() {
-                sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transitionDuration = ".5s";
-                sliders[indexOfSlider].translateValue += 20;
-                sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" + sliders[indexOfSlider].translateValue + "%)";
-                sliders[indexOfSlider].actual = 1;
-            }, 1);
-
-        } else {
-
-            sliders[indexOfSlider].translateValue = (-80 + (20 * (sliders[indexOfSlider].sliderItemsPerRow - 1)));
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" + sliders[indexOfSlider].translateValue + "%)";
-            sliders[indexOfSlider].actual = sliderItemsCount - sliders[indexOfSlider].reduceItemsCountForSliderItemsPerRow;
-
+        for (x = 0; x < sliderItemsCount; x++){
+            sliderItemsContainer.appendChild(sliderItemsOfThisSlider[x]);
+            sliderItemsOfThisSlider[x].classList.add("slider_items");
+            sliderItemsOfThisSlider[x].style.width = (100 / sliderItemsCount) + "%";
         }
 
-    }
+    //SLIDER METHODS
+        this.prev = function(){
+        
+            if (this.actual > 1){
 
-}
+                this.translateValue += 20;
+                sliderItemsContainer.style.transform = "translateX(" + this.translateValue + "%)";
+                this.actual--;
+        
+            } else if (this.actual == 1){
+        
+                if (this.infinite){
 
-function next(){
-
-    getInformationOfSlider.call(this);
-
-    if (sliders[indexOfSlider].actual < sliderItemsCount - sliders[indexOfSlider].reduceItemsCountForSliderItemsPerRow){
-
-        sliders[indexOfSlider].translateValue -= 20;
-        sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" +  sliders[indexOfSlider].translateValue + "%)";
-        sliders[indexOfSlider].actual++;
-
-    } else if (sliders[indexOfSlider].actual == sliderItemsCount - sliders[indexOfSlider].reduceItemsCountForSliderItemsPerRow){
-
-        if (sliders[indexOfSlider].infinite){
-
-            sliderItemsParent.appendChild(sliderItems[0]);
-            sliderItems = sliderParentOfClickedArrow.querySelectorAll(".slider_items");
-
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transitionDuration = "0s";
-            sliders[indexOfSlider].translateValue += 20;
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" +  sliders[indexOfSlider].translateValue + "%)";
-
-            setTimeout(function() {
-                sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transitionDuration = ".5s";
-                sliders[indexOfSlider].translateValue -= 20;
-                sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" +  sliders[indexOfSlider].translateValue + "%)";
-                sliders[indexOfSlider].actual = sliderItemsCount - sliders[indexOfSlider].reduceItemsCountForSliderItemsPerRow;
-            }, 1);
-            
-
-        } else {
-
-            sliders[indexOfSlider].translateValue = 0;
-            sliders[indexOfSlider].element.querySelector(".slider_items_container").style.transform = "translateX(" +  sliders[indexOfSlider].translateValue + "%)";
-            sliders[indexOfSlider].actual = 1;
-
+                    sliderItemsContainer.prepend(sliderItemsOfThisSlider[sliderItemsCount - 1]);
+                    sliderItemsOfThisSlider = this.element.querySelectorAll(".slider_items");
+        
+                    sliderItemsContainer.style.transitionDuration = "0s";
+                    this.translateValue -= 20;
+                    sliderItemsContainer.style.transform = "translateX(" + this.translateValue + "%)";
+        
+                    setTimeout(function() {
+                        sliderItemsContainer.style.transitionDuration = animationTime;
+                        self.translateValue += 20;
+                        sliderItemsContainer.style.transform = "translateX(" + self.translateValue + "%)";
+                        self.actual = 1;
+                    }, 1);
+        
+                } else {
+        
+                    this.translateValue = (-80 + (20 * (sliderItemsPerRow - 1)));
+                    sliderItemsContainer.style.transform = "translateX(" + this.translateValue + "%)";
+                    this.actual = sliderItemsCount - this.reduceItemsCountForSliderItemsPerRow;
+        
+                }
+        
+            }
+        
         }
 
-    }
-
-}
-
-function getInformationOfSlider(){
-
-    sliderParentOfClickedArrow = this.parentNode,
-    sliderItems = sliderParentOfClickedArrow.querySelectorAll(".slider_items"),
-    sliderItemsCount = sliderItems.length,
-    sliderItemsWidth = sliderItems[0].offsetWidth,
-    sliderItemsParent = sliderItems[0].parentNode,
-    indexOfSlider = -1;
-
-    for (var i = 0; i < sliders.length; i++) {
-
-        if (sliders[i].element === sliderParentOfClickedArrow) {
-            indexOfSlider = i;
-            break;
+        this.next = function(){
+        
+            if (this.actual < sliderItemsCount - this.reduceItemsCountForSliderItemsPerRow){
+        
+                this.translateValue -= 20;
+                this.element.querySelector(".slider_items_container").style.transform = "translateX(" +  this.translateValue + "%)";
+                this.actual++;
+        
+            } else if (this.actual == sliderItemsCount - this.reduceItemsCountForSliderItemsPerRow){
+        
+                if (this.infinite){
+        
+                    sliderItemsContainer.appendChild(sliderItemsOfThisSlider[0]);
+                    sliderItemsOfThisSlider = this.element.querySelectorAll(".slider_items");
+        
+                    sliderItemsContainer.style.transitionDuration = "0s";
+                    this.translateValue += 20;
+                    sliderItemsContainer.style.transform = "translateX(" +  this.translateValue + "%)";
+        
+                    setTimeout(function() {
+                        sliderItemsContainer.style.transitionDuration = animationTime;
+                        self.translateValue -= 20;
+                        sliderItemsContainer.style.transform = "translateX(" +  self.translateValue + "%)";
+                        self.actual = sliderItemsCount - self.reduceItemsCountForSliderItemsPerRow;
+                    }, 1);
+                    
+        
+                } else {
+        
+                    this.translateValue = 0;
+                    sliderItemsContainer.style.transform = "translateX(" +  this.translateValue + "%)";
+                    this.actual = 1;
+        
+                }
+        
+            }
+        
         }
 
-    }
+        function prevItem(){
+            self.prev();
+        }
+
+        function nextItem(){
+            self.next();
+        }
 
 }
